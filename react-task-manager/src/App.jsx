c70335c0588aca63ed54d4743c1ced783aef64f8
harshrajput2804 <161627226+harshrajput2804@users.noreply.github.com>
2025-12-17 22:15:29 +0530

    New Project Added Task Management

---
+++ react-task-manager/src/App.jsx
@@ -0,0 +1,11 @@
+import TaskList from "./components/TaskList"; 
+
+function App() {
+    return (
+        <div>
+          <h1>React Task Management</h1>
+          <TaskList />
+        </div>
+    );
+}
+export default App;
---
+++ react-task-manager/src/components/TaskList.jsx
@@ -0,0 +1,15 @@
+function TaskList() {
+    return (
+        <div>
+            <h1>My Tasks</h1><hr />
+            <ul>
+                <li>Learn React Basics</li>
+                <li>Understand Components</li>
+                <li>Bulid First Project</li>
+                <li>Add One more Task</li>
+                <li>Add Second Task</li>
+            </ul>
+        </div>
+    );
+}
+export default TaskList;
