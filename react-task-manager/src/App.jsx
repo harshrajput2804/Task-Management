643ff0a0120eda2370ee71b5ee10e4f50e3f35de
harshrajput2804 <161627226+harshrajput2804@users.noreply.github.com>
2025-12-18 13:19:39 +0530

    input to control tasks

--- react-task-manager/src/App.jsx
+++ react-task-manager/src/App.jsx
@@ -1,10 +1,19 @@
 import TaskList from "./components/TaskList"; 
 
 function App() {
+    // props to pass to TaskList component
+    const tasks = [
+        "Learn React Basics",
+        "Understand Components",
+        "Build First Project",
+        "Add One more Task",
+        "Add Second Task"
+    ];
+    
     return (
         <div>
           <h1>React Task Management</h1>
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
     );
 }
--- react-task-manager/src/components/TaskList.jsx
+++ react-task-manager/src/components/TaskList.jsx
@@ -1,14 +1,41 @@
-function TaskList() {
+import { useState } from "react";
+
+function TaskList({ tasks }) {
+    const [taskList, setTasksList] = useState(tasks);
+
+    // new task using input field
+    const [newTask, setNewTask] = useState("");
+
+    const addTask = () => {
+        if (newTask.trim() !== "") {
+            setTasksList([...taskList, newTask]);
+            setNewTask("");
+        }
+    };
     return (
         <div>
-            <h1>My Tasks</h1><hr />
-            <ul>
-                <li>Learn React Basics</li>
-                <li>Understand Components</li>
-                <li>Bulid First Project</li>
-                <li>Add One more Task</li>
-                <li>Add Second Task</li>
-            </ul>
+            <div className="section1">
+
+                <h2>My Tasks</h2><hr />
+
+                {/* input field to add new task when keyboard 'Enter' is pressed */}
+                <input
+                    type="text"
+                    placeholder="Enter New Task"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                />
+
+                <button onClick={addTask} disabled={!newTask.trim()}>
+                    Add Task
+                </button>
+
+                <ul>
+                    {taskList.map((task, index) => (
+                        <li key={index}>{task}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
